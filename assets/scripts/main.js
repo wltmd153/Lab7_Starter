046d6b46eab41a0203b6baae32fcca560c4844af
wltmd153 <103233907+wltmd153@users.noreply.github.com>
2022-11-14 22:58:41 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -47,12 +47,14 @@ function initializeServiceWorker() {
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
   if('serviceWorker' in navigator) {
     window.addEventListener('load', async function (){
+      try{
       const registration = await navigator.serviceWorker.register("./sw.js", {scope: "./",});
       if(register.active){
         console.log('successful, active');
       }
-      else{
-        console.log('failed, not active');
+      }
+      catch(error){
+        console.log('error');
       }
     });
   }
