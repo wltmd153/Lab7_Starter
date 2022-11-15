cf79e388d201a3244a81e9a7b9093b4d3d9c7fb2
wltmd153 <103233907+wltmd153@users.noreply.github.com>
2022-11-14 23:14:16 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -44,18 +44,7 @@ self.addEventListener('fetch', function (event) {
   // B8. TODO - If the request is in the cache, return with the cached version.
   //            Otherwise fetch the resource, add it to the cache, and return
   //            network response.
-   event.respondWith(caches.open(CACHE_NAME).then((cache) => {
-      // Respond with the image from the cache or from the network
-      return cache.match(event.request).then((cachedResponse) => {
-        return cachedResponse || fetch(event.request.url).then((fetchedResponse) => {
-          // Add the network response to the cache for future visits.
-          // Note: we need to make a copy of the response to save it in
-          // the cache and use the original as the request response.
-          cache.put(event.request, fetchedResponse.clone());
-
-          // Return the network response
-          return fetchedResponse;
-        });
-  });
-});
+   
+      
+      
 });
